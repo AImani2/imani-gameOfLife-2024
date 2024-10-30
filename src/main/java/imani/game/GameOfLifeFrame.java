84078b32786d6953a75ac5635695008c1bd12a65
AImani2 <121688634+AImani2@users.noreply.github.com>
2024-10-30 09:53:47 -0400

    more checkstyles

--- src/main/java/imani/game/GameOfLifeFrame.java
+++ src/main/java/imani/game/GameOfLifeFrame.java
@@ -9,22 +9,8 @@ import javax.swing.JFrame;
 
 public class GameOfLifeFrame extends JFrame {
 
-    //private final GameOfLifeComponent gridComponent;
-
-    //private RleImporter rleImporter = new RleImporter();
-
     public GameOfLifeFrame() {
 
-//        int gridWidth = Math.max(grid.getGameBoard().length, 100);
-//        int gridHeight = Math.max(grid.getGameBoard()[0].length, 100);
-//
-//        pack();
-//        Insets insets = getInsets();
-//
-//        int cellSize = 9;
-//
-//        int width = (gridWidth * cellSize) + insets.left + insets.right;
-//        int height = (gridHeight * cellSize) + insets.top + insets.bottom + buttons.getPreferredSize().height;
         setSize(800, 600);
         setTitle("Conway's Game of Life");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
